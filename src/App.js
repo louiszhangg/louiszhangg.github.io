11b4d784fd9666a44d32d29e30e42b8bc54c7dbe
Louis Zhang <louiszhang@louiss-air.lan>
2024-06-17 23:49:38 -0400

    Cursor actually works now (just for clicking)

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+function App() {
+  const [cursorPos, setCursorPos] = useState({
+    x: 0,
+    y: 0,
+  });
+  // const [deviceType, setDeviceType] = useState("");
+  const [isClicking, setIsClicking] = useState(false);
+  // const [hovering, setHovering] = useState(false);
+
+  // const isTouchDevice = () => {
+  //   try {
+  //     document.createEvent("TouchEvent");
+  //     setDeviceType("touch");
+  //     return true;
+  //   } catch (e) {
+  //     setDeviceType("mouse");
+  //     return false;
+  //   }
+  // };
+
+  const cursorMoved = (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+ 
+    setCursorPos({
+      x: x,
+      y: y,
+    })
+  };
+
+  const handleMouseDown = () => {
+    setIsClicking(true);
+  }
+
+  const handleMouseUp = () => {
+    setIsClicking(false);
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousemove", cursorMoved);
+    document.addEventListener("mousedown", handleMouseDown);
+    document.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      document.removeEventListener("mousemove", cursorMoved);
+      document.removeEventListener("mousedown", handleMouseDown);
+      document.removeEventListener("mouseup", handleMouseUp);
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <div
+        className="cursor"
+        style={{ 
+          left: `${cursorPos.x}px`, 
+          top: `${cursorPos.y}px`, 
+          height: `${isClicking ? '17px' : '20px'}`,
+          width: `${isClicking ? '17px' : '20px'}`,
+          opacity: `${isClicking ? 0.85 : 0.6}`,
+        }}
+      ></div>
+      <h1 className="title">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
